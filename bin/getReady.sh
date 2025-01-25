8c7c31c75b5beb7c3c6343213a310c3745181ed0
rashad <rashadalsharpini@gmail.com>
2025-01-25 09:57:42 +0200

    update

--- bin/getReady.sh
+++ bin/getReady.sh
@@ -3,5 +3,7 @@ xmodmap -e "keycode 135 = Super_R"
 setxkbmap -option caps:escape
 variety &
 xfce4-clipman &
-~/.local/bin/monitor.sh
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-4 --off --output HDMI-1 --off --output DP-5 --off --output HDMI-2 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-6 --off --output HDMI-3 --off --output VGA-1-0 --off --output DP-1-0 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off
+blueberry &
+exit 0
+# ~/.local/bin/monitor.sh
+# xrandr --output eDP-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-4 --off --output HDMI-1 --off --output DP-5 --off --output HDMI-2 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-6 --off --output HDMI-3 --off --output VGA-1-0 --off --output DP-1-0 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off
